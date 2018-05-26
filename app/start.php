d17ee980f80ef117a65052723f9caf3300571e33
b3n10 <francisben.lleve@gmail.com>
2018-05-26 21:45:32 +0800

    Create instance Slim

--- app/start.php
+++ app/start.php
@@ -1,6 +1,11 @@
 <?php
 
-use Slim/Slim;
+use Slim\Slim;
 
 // require composer autoloader
 require_once '../vendor/autoload.php';
+
+// create instance of Slim class
+// another way:
+// $app = new \Slim\Slim();
+$app = new Slim();
