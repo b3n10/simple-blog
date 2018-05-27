c1a2f26721d509682ad55b3ad105a81d98c3b552
b3n10 <francisben.lleve@gmail.com>
2018-05-27 19:47:58 +0800

    Add Twig and Twig extension on $view obj

--- app/start.php
+++ app/start.php
@@ -8,7 +8,10 @@ require_once '../vendor/autoload.php';
 // create instance of Slim class
 // another way:
 // $app = new \Slim\Slim();
-$app = new Slim();
+$app = new Slim([
+	// choose twig for $view obj
+	'view'	=>	new \Slim\Views\Twig()
+]);
 
 // Database
 $app->container->singleton('db', function() {
@@ -21,6 +24,11 @@ $view = $app->view();
 
 // set the View templates dir
 $view->setTemplatesDirectory('../app/views');
+
+// find parser extensions for $view
+$view->parserExtensions = [
+	new \Slim\Views\TwigExtension()
+];
 
 // require routes
 require_once 'routes.php';
