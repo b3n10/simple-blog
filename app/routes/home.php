edf7e1c7b7fb7357c16006be6b7fd3b84ad87547
b3n10 <francisben.lleve@gmail.com>
2018-05-27 18:01:58 +0800

    SQL from posts table

--- app/routes/home.php
+++ app/routes/home.php
@@ -4,4 +4,13 @@
 // use ($app) to include $app obj inside closure function
 $app->get('/', function() use ($app) {
 
+	// query all from posts table where posts.user_id = users.id
+	$posts = $app->db->query("
+		SELECT
+		posts.*
+		FROM posts
+		LEFT JOIN users
+		ON posts.user_id = users.id
+	")->fetchAll(PDO::FETCH_ASSOC); // as associative array
+
 })->name('home'); // name of the route
