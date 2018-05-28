74ea5f82cdbbb3dbffc20b65944c75146eec494c
b3n10 <francisben.lleve@gmail.com>
2018-05-28 13:43:04 +0800

    Add comments

--- app/routes/posts/show.php
+++ app/routes/posts/show.php
@@ -1,4 +1,6 @@
 <?php
+// make sure to create .htaccess in public/ dir
+// see: https://stackoverflow.com/a/30633620/6353682
 
 // get the /posts/ dir and pass postID variable
 $app->get('/posts/:postID', function($postID) use ($app) {
